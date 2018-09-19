0fced6529ae004c3afc9c17b3a516f827f790010
aelane <aelane@berkeley.edu>
2018-09-19 18:12:57 -0500

    Introduce file structure with all of the folders

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Final Project for Fall 2018 MIDS W266 NLP with Deep Learning',
+    author='aelane',
+    license='',
+)
